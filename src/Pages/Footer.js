1eab1c97a48cf58200ae011e9da6fb39823fc937
Lamilla19 <57071153+Lamilla19@users.noreply.github.com>
2021-07-23 20:34:27 -0500

    nuevos cambios

---
+++ src/Pages/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="cont-footer">
+      <div className="text-footer">
+        <p>PF</p>
+        <p>Portafolio Anthony </p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
--- src/Pages/Inicio.js
+++ src/Pages/Inicio.js
@@ -1,8 +1,10 @@
 import React from "react";
 import Logos from "../Componentes/Logos";
 import Banner from "../Secciones/Banner";
+
 import Herramientas from "../Secciones/Herramientas";
 import Presentacion from "../Secciones/Presentacion";
+import Footer from "./Footer";
 import Proyectos from "./Proyectos";
 function Inicio() {
   return (
@@ -22,7 +24,9 @@ function Inicio() {
       <section className="Herramientas fondo section">
         <Herramientas />
       </section>
-      <section className="Footer"></section>
+      <section className="Footer">
+        <Footer />
+      </section>
     </main>
   );
 }
